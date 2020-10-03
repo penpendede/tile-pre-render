13a5ba30bc2757caa46034a5e2eb34d87c2e3ee7
Josef Schugt <penpendede@mail.ru>
2020-10-03 22:02:54 +0200

    addedd args to exampe

--- scripts/bonn-range.sh
+++ scripts/bonn-range.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
-node src/show_tile_range.js	\
-  --longitude-minimum=7.0225	\
-  --longitude-maximum=7.2107	\
-  --latitude-minimum=50.6326	\
-  --latitude-maximum=50.7745	\
-  --zoom-minimum=0		\
-  --zoom-maximum=18
+node src/generate_render_script.js \
+  --x-min=7.0225 --y-min=50.6326 \
+  --x-max=7.2107 --y-max=50.7745 \
+  --z-min=1 --z-max=18 \
+  --proj=EPSG:4326 \
+  --shell=bash \
+  --file=scripts/populate_bonn.sh --overwrite \
+  --command=render_list \
+  --map=osm \
+  --socket=/run/renderd/renderd.sock \
+  --threads=4 \
+  --force \
+  --load=16 \
+  --tile-dir=/var/lib/mod_tile
+ 
