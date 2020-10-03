d0d14102085c7f289e3cdba887facbe12637de50
Josef Schugt <penpendede@mail.ru>
2020-10-03 19:53:11 +0200

    script cleanup/streamlining

--- src/show_tile_range.js
+++ src/show_tile_range.js
@@ -4,24 +4,24 @@ const sprintf = require('sprintf-js').sprintf
 let h
 
 const commandLineArguments = [
-  ['x', 'x-val-min=ARG', 'lower limit for x value'],
-  ['X', 'x-val-max=ARG', 'upper limit for x value'],
-  ['y', 'y-val-min=ARG', 'lower limit for y value'],
-  ['Y', 'y-val-max=ARG', 'upper limit for y value'],
-  ['z', 'zoom-min=ARG', 'lowest zoom level value'],
-  ['Z', 'zoom-max=ARG', 'highest zoom level value'],
+  ['x', 'x-val-lower=ARG', 'lower limit for x value'],
+  ['X', 'x-val-upper=ARG', 'upper limit for x value'],
+  ['y', 'y-val-lower=ARG', 'lower limit for y value'],
+  ['Y', 'y-val-upper=ARG', 'upper limit for y value'],
+  ['z', 'z-val-lower=ARG', 'lower limit for z value (zoom level)'],
+  ['Z', 'z-val-upper=ARG', 'upper limit for z value (zoom level)'],
   ['p', 'projection=ARG', 'projection in which the coordinates are provided'],
   ['h', 'help', 'display this help']
 ]
 
-function toRadians (w) {
+function toRad (w) {
   return w * Math.PI / 180.0
 }
 
 function getTileCoordinates (lat, lon, zoom) {
   let xTile = Math.floor((lon + 180) / 360 * Math.pow(2, zoom))
   let yTile = Math.floor(
-    (1.0 - Math.log(Math.tan(toRadians(lat)) + 1.0 / Math.cos(toRadians(lat))) / Math.PI) * Math.pow(2.0, zoom - 1.0)
+    (1.0 - Math.log(Math.tan(toRad(lat)) + 1.0 / Math.cos(toRad(lat))) / Math.PI) * Math.pow(2.0, zoom - 1.0)
   )
   if (xTile < 0.0) {
     xTile = 0.0
@@ -41,131 +41,51 @@ function getTileCoordinates (lat, lon, zoom) {
   }
 }
 
-const opt = require('node-getopt').create(commandLineArguments)
-  .bindHelp()
-  .parseSystem()
+const opt = require('node-getopt').create(commandLineArguments).bindHelp().parseSystem()
 
-let xMin, xMax, yMin, yMax, zMin, zMax, projection
+const args = {}
 
-// Handle y value minimum
-if (!Object.hasOwnProperty.call(opt.options, 'y-val-min')) {
-  console.error('no lower limit for y value provided.')
-  process.exit(1)
-} else {
-  yMin = Number.parseFloat(opt.options['y-val-min'])
-  if (Number.isNaN(yMin)) {
-    console.error('lower limit for y value cannot be parsed as a number.')
-    process.exit(1)
+for (const id of ['x', 'y', 'z']) {
+  for (const limit of ['lower', 'upper']) {
+    if (!Object.hasOwnProperty.call(opt.options, `${id}-val-${limit}`)) {
+      console.error(`no ${limit} for ${id} value provided.`)
+      process.exit(1)
+    } else {
+      args[`${id}-val-${limit}`] = Number.parseFloat(opt.options[`${id}-val-${limit}`])
+      if (Number.isNaN(args[id])) {
+        console.error(`lower limit for ${id} value cannot be parsed as a number.`)
+        process.exit(1)
+      }
+    }
   }
-}
-
-// Handle y value maximum
-if (!Object.hasOwnProperty.call(opt.options, 'y-val-max')) {
-  console.error('no upper limit for y value provided.')
-  process.exit(1)
-} else {
-  yMax = Number.parseFloat(opt.options['y-val-max'])
-  if (Number.isNaN(yMax)) {
-    console.error('upper limit for y value cannot be parsed as a number.')
-    process.exit(1)
+  if (args[`${id}-val-lower`] > args[`${id}-val-upper`]) {
+    console.log(`lower limit for ${id} value higher than uppz-val-lowerer limit, swapping`)
+    h = args[`${id}-val-lower`]
+    args[`${id}-val-lower`] = args[`${id}-val-upper`]
+    args[`${id}-val-upper`] = h
   }
-}
-
-// Ensure y value minimum <= y value maximum
-if (yMin > yMax) {
-  console.log('lower limit for y value higher than upper limit, swapping')
-  h = yMin
-  yMin = yMax
-  process.exit(1)
-}
-
-// Handle x value minimum
-if (!Object.hasOwnProperty.call(opt.options, 'x-val-min')) {
-  console.error('no lower limit for x value provided.')
-  process.exit(1)
-} else {
-  xMin = Number.parseFloat(opt.options['x-val-min'])
-  if (Number.isNaN(xMin)) {
-    console.error('lower limit for x value cannot be parsed as a number.')
-    process.exit(1)
-  }
-}
-
-// Handle x value maximum
-if (!Object.hasOwnProperty.call(opt.options, 'x-val-max')) {
-  console.error('no upper limit for x value provided.')
-  process.exit(1)
-} else {
-  xMax = Number.parseFloat(opt.options['x-val-max'])
-  if (Number.isNaN(xMax)) {
-    console.error('upper limit for x value cannot be parsed as a number.')
-    process.exit(1)
-  }
-}
-
-// Ensure x value minimum <= x value maximum
-if (xMin > xMax) {
-  console.log('lower limit for x value higher than upper limit, swapping')
-  h = xMin
-  xMin = xMax
-  xMax = h
 }
 
 if (!Object.hasOwnProperty.call(opt.options, 'projection')) {
-  projection = 'EPSG:4326'
+  args.projection = 'EPSG:4326'
 } else {
-  projection = opt.options.projection
+  args.projection = opt.options.projection
 }
 
-if (projection !== 'EPSG:4326') {
-  var convertedCoordinates = proj4(projection, 'EPSG:4326', [xMin, yMin])
-  xMin = convertedCoordinates[0]
-  yMin = convertedCoordinates[1]
-  convertedCoordinates = proj4(projection, 'EPSG:4326', [xMax, yMax])
-  xMax = convertedCoordinates[0]
-  yMax = convertedCoordinates[1]
-}
-
-// Handle zoom-min
-if (!Object.hasOwnProperty.call(opt.options, 'zoom-min')) {
-  console.error('no lower limit for zoom provided.')
-  process.exit(1)
-} else {
-  zMin = Number.parseInt(opt.options['zoom-min'])
-  if (Number.isNaN(zMin)) {
-    console.error('lower limit for zoom cannot be parsed as a number.')
-    process.exit(1)
+if (args.projection !== 'EPSG:4326') {
+  for (const limit of ['lower', 'upper']) {
+    var convertedCoordinates = proj4(args.projection, 'EPSG:4326', [args[`x-val-${limit}`], args[`y-val-${limit}`]])
+    args[`x-val-${limit}`] = convertedCoordinates[0]
+    args[`y-val-${limit}`] = convertedCoordinates[1]
   }
 }
-
-// Handle zoom maximum
-if (!Object.hasOwnProperty.call(opt.options, 'zoom-maximum')) {
-  console.error('no upper limit for zoom provided.')
-  process.exit(1)
-} else {
-  zMax = Number.parseInt(opt.options['zoom-maximum'])
-  if (Number.isNaN(zMax)) {
-    console.error('upper limit for zoom cannot be parsed as a number.')
-    process.exit(1)
-  }
-}
-
-// Ensure x value minimum <= x value maximum
-if (zMin > zMax) {
-  console.log('lower limit for zoom higher than upper limit, swapping')
-  h = zMin
-  zMin = zMax
-  zMax = h
-}
-
-for (let zoom = zMin; zoom <= zMax; zoom++) {
-  let coords = getTileCoordinates(yMin, xMin, zoom)
+console.log(args)
+for (let z = args['z-val-lower']; z <= args['z-val-upper']; z++) {
+  let coords = getTileCoordinates(args['y-val-lower'], args['x-val-lower'], z)
   const x0 = coords.X
   const y0 = coords.Y
-  coords = getTileCoordinates(yMax, xMax, zoom)
+  coords = getTileCoordinates(args['y-val-upper'], args['x-val-upper'], z)
   const x1 = coords.X
   const y1 = coords.Y
   console.log(sprintf('zoom %7d: -x %7d -X %7d -y %7d -Y %7d', zoom, x0, x1, y1, y0))
 }
-
-process.exit(0)
