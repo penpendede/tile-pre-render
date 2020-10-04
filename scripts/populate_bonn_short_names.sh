fd709a2ce9d847a60b288643a1a305b2ce7b3c76
Josef Schugt <penpendede@mail.ru>
2020-10-04 14:23:05 +0200

    two examples now, one with short argument names, one with long ones

---
+++ scripts/populate_bonn_short_names.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 1 -Z 1 -x 1 -X 1 -y 0 -Y 0
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 2 -Z 2 -x 2 -X 2 -y 1 -Y 1
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 3 -Z 3 -x 4 -X 4 -y 2 -Y 2
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 4 -Z 4 -x 8 -X 8 -y 5 -Y 5
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 5 -Z 5 -x 16 -X 16 -y 10 -Y 10
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 6 -Z 6 -x 33 -X 33 -y 21 -Y 21
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 7 -Z 7 -x 66 -X 66 -y 42 -Y 43
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 8 -Z 8 -x 132 -X 133 -y 85 -Y 86
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 9 -Z 9 -x 265 -X 266 -y 171 -Y 172
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 10 -Z 10 -x 531 -X 532 -y 343 -Y 344
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 11 -Z 11 -x 1063 -X 1065 -y 687 -Y 688
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 12 -Z 12 -x 2127 -X 2130 -y 1375 -Y 1377
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 13 -Z 13 -x 4255 -X 4260 -y 2750 -Y 2755
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 14 -Z 14 -x 8511 -X 8520 -y 5501 -Y 5511
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 15 -Z 15 -x 17023 -X 17040 -y 11002 -Y 11022
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 16 -Z 16 -x 34046 -X 34080 -y 22005 -Y 22045
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 17 -Z 17 -x 68092 -X 68161 -y 44010 -Y 44091
+render_list -a  -s /run/renderd/renderd.sock -f  -m  osm -n  4 -z 18 -Z 18 -x 136185 -X 136322 -y 88020 -Y 88183
