fa92783270fb0f3b9285610889a5f3efc2fae251
Josef Schugt <penpendede@mail.ru>
2020-10-08 22:23:32 +0200

    updated examples

--- scripts/bonn-range.sh
+++ scripts/bonn-range.sh
@@ -2,7 +2,7 @@
 node src/generate_render_script/index.js \
   --x-min=7.0225 --y-min=50.6326 \
   --x-max=7.2107 --y-max=50.7745 \
-  --z-min=1 --z-max=18 \
+  --z-min=0 --z-max=18 \
   --proj=EPSG:4326 \
   --shell=bash \
   --file=scripts/populate_bonn_short_names.sh --overwrite \
@@ -17,7 +17,7 @@ node src/generate_render_script/index.js \
 node src/generate_render_script/index.js \
   --x-min=7.0225 --y-min=50.6326 \
   --x-max=7.2107 --y-max=50.7745 \
-  --z-min=1 --z-max=18 \
+  --z-min=0 --z-max=18 \
   --proj=EPSG:4326 \
   --shell=bash \
   --file=scripts/populate_bonn_long_names.sh --overwrite \
